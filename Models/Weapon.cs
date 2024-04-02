b8dbd0a2916999e5e11d25c35592bffcd5e1221e
AsadovIbrahim <ibrahimasadov31@gmail.com>
2024-04-02 09:16:51 +0400

    commit

---
+++ Models/Weapon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Practic.Models
+{
+    public class Weapon
+    {
+		private int _Capacity;
+		private int CurrentBullet;
+		private bool isAutomatic;
+
+		public int Capacity
+		{
+			get { return _Capacity; }
+			set { _Capacity = value; }
+		}
+		
+		public Weapon(int capacity,bool IsAutomatic)
+		{
+			Capacity = capacity;
+			CurrentBullet = 0;
+			this.isAutomatic=IsAutomatic;
+		}
+		public void Shoot()
+		{
+			if(CurrentBullet> 0) {
+				if (!isAutomatic)
+				{
+					Console.WriteLine("1 shot fired...");
+					CurrentBullet--;
+				}
+				else
+				{
+					Console.WriteLine("Automatic shot fired...");
+					CurrentBullet = 0;
+				}
+			}
+			else
+			{
+				GetReload();
+				Console.WriteLine("Weapon Updated...");
+			}
+		}
+		public int GetRemainBulletCount()
+		{
+			return Capacity - CurrentBullet;
+		}
+		public void GetReload()
+		{
+			CurrentBullet = Capacity;
+		}
+		public void ChangeFireMode()
+		{
+			isAutomatic = !isAutomatic;
+			Console.WriteLine("FireMode is Changed "+(isAutomatic?"Automatic":"Single"));
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using ConsoleApp_Practic.Models;
+
+namespace ConsoleApp_Practic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = null;
+
+            while (true)
+            {
+                Console.WriteLine("0.Get Information");
+                Console.WriteLine("1.To Shot");
+                Console.WriteLine("2.ShowRemain Bullet Count");
+                Console.WriteLine("3.To Reload");
+                Console.WriteLine("4.To Change Fire Mode");
+                Console.WriteLine("5.Exit");
+
+                int choice=Convert.ToInt32(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 0:
+                        if(weapon== null)
+                        {
+                            Console.WriteLine("Weapon Is Not Created!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{weapon.GetRemainBulletCount()}");
+                        }
+                        break;
+                    case 1:
+                        if (weapon == null)
+                        {
+                            Console.WriteLine("Add Weapon:");
+                            Console.Write("Enter BulletCount:");
+                            int capacity=Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter Fire Mode:\nfalse:Single\ntrue:Automatic");
+                            bool isAutomaTic=Convert.ToBoolean(Console.ReadLine());
+                            weapon=new Weapon(capacity, isAutomaTic);
+                        }
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        Console.WriteLine($"Remain Bullet Count is:{weapon!.GetRemainBulletCount()}");
+                        break;
+                    case 3:
+                        weapon!.GetReload();
+                        Console.WriteLine("Gun Is Reloaded...");
+                        break;
+                    case 4:
+                        weapon!.ChangeFireMode();
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice!!");
+                        break;
+                }
+            }
+        }
+    }
+}
